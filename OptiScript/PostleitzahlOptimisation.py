8bda6a08282237481faf9067fae148e3dd7cbe05
FoxPilot05 <jonsebad@gmail.com>
2024-11-12 10:39:17 +0100

    Visual Improovements on html

--- OptiScript/PostleitzahlOptimisation.py
+++ OptiScript/PostleitzahlOptimisation.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def home():
-    return render_template('index2.html')
+    return render_template('index4.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
