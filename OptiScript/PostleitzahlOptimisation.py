efd1ceefaca1fabd2142ccf704caf66a38944bca
FoxPilot05 <jonsebad@gmail.com>
2024-11-13 11:05:58 +0100

    about us page

--- OptiScript/PostleitzahlOptimisation.py
+++ OptiScript/PostleitzahlOptimisation.py
@@ -11,5 +11,9 @@ def home():
 def newpage():
     return render_template('results.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
